e8a71bcea679547e8cd6d6e29653685a88c742e9
Jackline Jebet <jebetjackline2016@gmail.com>
2022-06-08 15:12:08 +0300

    commit tree e81327e8d069d3e929405328ea72af11775c2b22

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function ( drivers) {
+  return drivers.slice(0,2); 
+}
+
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2)
+}
+
+const selectingDrivers = 
+[returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function (multilyValue) {
+    return function (value) {
+        return multilyValue * value;
+    }
+}
+
+function fareDoubler(fare) {
+    return fare*2
+}
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers (arraysOfDrivers, returnDrivers) {
+    let selectingDrivers = returnDrivers(arraysOfDrivers);
+    return selectingDrivers;
+}
